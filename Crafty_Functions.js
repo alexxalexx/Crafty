5329807277a1fcfed026c80b72631d89b75950b4
alexxalexx <alexxkoltakovv@gmail.com>
2022-02-19 20:49:09 +0300

    Update 20:49

---
+++ Crafty_Functions.js
@@ -0,0 +1,11 @@
+const panel_button_a = document.getElementById('panel_button_a')
+const panel_button_b = document.getElementById('panel_button_b')
+
+panel_button_a.onclick = function()
+{
+    alert('Im glad that you want to visit another page, but it doesnt exist yet... Sorry')
+}
+panel_button_b.onclick = function()
+{
+    alert('Well, it supposed to bring you back but this function is not done yet')
+}
